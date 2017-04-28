8c9e43e7701aff91206288a34c794820959c29cd
doubleflying <doublefly@aliyun.com>
2017-04-28 17:25:18 +0800

    首页初始化

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AppPonsive | Portfolio Landing Page' });
 });
 
 module.exports = router;
